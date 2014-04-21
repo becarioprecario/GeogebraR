a612a34020eeebb371bf432f10eae010c047fb26
Virgilio <Virgilio.Gomez@uclm.es>
2014-04-21 14:36:14 +0200

    Example on Quality Cntrol Charts

---
+++ R/qcc.R
@@ -0,0 +1,30 @@
+#Functions and data for the example on quality control charts
+
+#Sample observations
+qccsample<-function(n=20, mean=0, sd=1)
+{
+	qccpts<-rnorm(n=n, mean=mean, sd=sd)
+
+	return(qccpts)
+}
+
+
+charlimits<-function(qccpts=NULL)
+{
+	require(qcc)
+
+	if(!is.null(qccpts))
+	{
+		cchart<-qcc(matrix(qccpts, ncol=4), type="xbar", plot=FALSE)
+		return(list(lowlim=cchart$limits[1], uplim=cchart$limits[2]))
+	}
+	else
+	{
+		return(list(lowlim=NA, uplim=NA))
+	}
+}
+
+updateqcc<-function()
+{
+
+}
