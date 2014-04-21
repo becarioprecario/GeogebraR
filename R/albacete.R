654155a338fcf70cf5dacd14f851c1b8b1b56365
Virgilio <Virgilio.Gomez@uclm.es>
2014-04-22 01:22:10 +0200

    Example on how to import a map from R.

---
+++ R/albacete.R
@@ -0,0 +1,19 @@
+#Return boundaries of the municipalities in Albacete
+
+abmap<-function(){
+
+	require(sp)
+
+	data(albacete)
+
+	bb<-bbox(albacete)[,1]#Minimum coordinates
+	rescal<-50000#Re-scale factor
+
+	polys<-lapply(albacete@polygons, function(X){
+		xx<-X@Polygons[[1]]@coords/rescal#Coords. are re-scaled
+		xx<-xx-matrix(bb/rescal, nrow=nrow(xx), ncol=2, byrow=TRUE)
+		return(xx)
+	})
+
+	return(polys)
+}
