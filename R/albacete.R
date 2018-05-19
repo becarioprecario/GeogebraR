2bb7b95090e6229d26e7848e495981c776c376c0
Virgilio <VIrgilio.Gomez@uclm.es>
2018-05-19 21:05:03 +0200

    unionSP used to create outer boundary.

--- R/albacete.R
+++ R/albacete.R
@@ -1,24 +1,28 @@
-#Return boundaries of the municipalities in Albacete
+# Return boundaries of the municipalities in Albacete
 
-plotmap<-function(map="albacete"){
+plotmap <- function(map = "albacete"){
 
-	require(sp)
+  require(sp)
+  require(maptools)
 
-	data(map, envir=environment())#, package="GeogebraR")
+  data(map, envir = environment())#, package="GeogebraR")
 
-	if(map=="albacete"){xxmap<-albacete}
-	if(map=="granada"){xxmap<-granada}
-	if(map=="jaen"){xxmap<-jaen}
+  if(map == "albacete"){xxmap <- albacete}
+  if(map == "granada"){xxmap <- granada}
+  if(map == "jaen"){xxmap <- jaen}
 
-	#bb<-bbox(xxmap)[,1]#Minimum coordinates
-	bb<-c(10.20520, 84.17414)#Offset para colocar Albacete en el origen
-	rescal<-50000#Re-scale factor
+  # Get outer boundary
+  xxmap <- unionSpatialPolygons(xxmap, rep("1", length(xxmap)))
 
-	polys<-lapply(xxmap@polygons, function(X){
-		xx<-X@Polygons[[1]]@coords/rescal#Coords. are re-scaled
-		xx<-xx-matrix(bb, nrow=nrow(xx), ncol=2, byrow=TRUE)
-		return(xx)
-	})
+  #bb<-bbox(xxmap)[,1]#Minimum coordinates
+  bb <- c(10.20520, 84.17414)#Offset para colocar Albacete en el origen
+  rescal <- 50000#Re-scale factor
 
-	return(polys)
+  polys <- lapply(xxmap@polygons, function(X) {
+    xx <- X@Polygons[[1]]@coords / rescal#Coords. are re-scaled
+    xx <- xx - matrix(bb, nrow = nrow(xx), ncol = 2, byrow = TRUE)
+    return(xx)
+  })
+
+  return(polys)
 }
