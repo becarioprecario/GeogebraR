17910cc98b08509e989fa62756a3146340bf49ea
Virgilio <VIrgilio.Gomez@uclm.es>
2018-05-19 12:47:18 +0200

    Style.

--- R/qcc.R
+++ R/qcc.R
@@ -1,25 +1,25 @@
-#Functions and data for the example on quality control charts
+# Functions and data for the example on quality control charts
 
-#Sample observations
-qccsample<-function(n=20, mean=0, sd=1)
+# Sample observations
+qccsample <- function(n = 20, mean = 0, sd = 1)
 {
-	qccpts<-rnorm(n=n, mean=mean, sd=sd)
+  qccpts <- rnorm(n = n, mean = mean, sd = sd)
 
-	return(qccpts)
+  return(qccpts)
 }
 
 
-charlimits<-function(qccpts=NULL)
+charlimits <- function(qccpts = NULL)
 {
-	require(qcc)
+  require(qcc)
 
-	if(!is.null(qccpts))
-	{
-		cchart<-qcc(matrix(qccpts, ncol=4), type="xbar", plot=FALSE)
-		return(list(lowlim=cchart$limits[1], uplim=cchart$limits[2]))
-	}
-	else
-	{
-		return(list(lowlim=NA, uplim=NA))
-	}
+  if(!is.null(qccpts))
+  {
+    cchart <- qcc(matrix(qccpts, ncol = 4), type = "xbar", plot = FALSE)
+    return(list(lowlim = cchart$limits[1], uplim = cchart$limits[2]))
+  }
+  else
+  {
+    return(list(lowlim = NA, uplim = NA))
+  }
 }
